fdf84a9bcb430c04fb08092bbfc1e19aec4de207
David Menendez <dmenend@us.ibm.com>
2017-11-01 14:50:12 -0500

    added table sorting

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,26 +1,58 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Table = ({ data, cols }) => (
-  <table>
-    <thead>
-      <tr>
-        {cols.map(col => <th>{col}</th>)}
-      </tr>
-    </thead>
-    <tbody>
-      {data.map(d => (
-        <tr>
-          {cols.map(c => <td>{d[c]}</td>)}
-        </tr>
-      ))}
-    </tbody>
-  </table>
-);
+export default class Table extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: props.data,
+      sortAsc: true,
+      sortCol: null,
+    };
+    this.getClassName = this.getClassName.bind(this);
+    this.sort = this.sort.bind(this);
+  }
+
+  getClassName(col) {
+    return this.state.sortCol === col ? `sort sort--${this.state.sortAsc ? 'up' : 'down'}` : '';
+  }
+
+  sort(e) {
+    const targetCol = e.target.innerText;
+    const sortAsc = targetCol !== this.state.sortCol ? true : !this.state.sortAsc;
+    const data = this.state.data.slice().sort((a, b) => (
+      sortAsc ? a[targetCol] > b[targetCol] : a[targetCol] < b[targetCol]
+    ));
+    this.setState({
+      data,
+      sortAsc,
+      sortCol: targetCol,
+    });
+  }
+
+  render() {
+    return (
+      <table className="dataTable">
+        <thead>
+          <tr>
+            {this.props.cols.map(col => (
+              <th className={this.getClassName(col)} onClick={this.sort}>{col}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {this.state.data.map(d => (
+            <tr>
+              {this.props.cols.map(c => <td>{d[c]}</td>)}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+}
 
 Table.propTypes = {
   data: PropTypes.arrayOf(PropTypes.object).isRequired,
   cols: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
-
-export default Table;

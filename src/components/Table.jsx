016fa2ecbd52fd6f5b23b1f33c97ac9fccc28562
David Menendez <dmenend@us.ibm.com>
2017-11-01 15:18:26 -0500

    move out table data state

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -5,43 +5,47 @@ export default class Table extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: props.data,
       sortAsc: true,
       sortCol: null,
     };
     this.getClassName = this.getClassName.bind(this);
-    this.sort = this.sort.bind(this);
+    this.setSort = this.setSort.bind(this);
   }
 
   getClassName(col) {
     return this.state.sortCol === col ? `sort sort--${this.state.sortAsc ? 'up' : 'down'}` : '';
   }
 
-  sort(e) {
+  setSort(e) {
     const targetCol = e.target.innerText;
     const sortAsc = targetCol !== this.state.sortCol ? true : !this.state.sortAsc;
-    const data = this.state.data.slice().sort((a, b) => (
-      sortAsc ? a[targetCol] > b[targetCol] : a[targetCol] < b[targetCol]
-    ));
     this.setState({
-      data,
       sortAsc,
       sortCol: targetCol,
     });
   }
 
   render() {
+    const {
+      sortAsc,
+      sortCol,
+    } = this.state;
+
+    const tableData = this.props.data.slice().sort((a, b) => (
+      sortAsc ? a[sortCol] > b[sortCol] : a[sortCol] < b[sortCol]
+    ));
+
     return (
       <table className="dataTable">
         <thead>
           <tr>
             {this.props.cols.map(col => (
-              <th className={this.getClassName(col)} onClick={this.sort}>{col}</th>
+              <th className={this.getClassName(col)} onClick={this.setSort}>{col}</th>
             ))}
           </tr>
         </thead>
         <tbody>
-          {this.state.data.map(d => (
+          {tableData.map(d => (
             <tr>
               {this.props.cols.map(c => <td>{d[c]}</td>)}
             </tr>
